b8410d24cdcc93bba7ed68419aefd75ee00730c1
vinay02yadav <02vyadav@gmail.com>
2026-05-31 05:49:09 +0530

    Fix port mapping to use port 80

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://35.208.254.175/:path*", 
+      },
+    ];
+  },
 };
 
 export default nextConfig;
